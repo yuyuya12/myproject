124fbd9e74bbf90c0a6e6438905b8480b1e1ba4e
yuyuya12 <1803411298@qq.com>
2019-09-10 20:45:37 +0800

    书写并完善了购物车页面

--- jq/nav-er.js
+++ jq/nav-er.js
@@ -1,6 +1,6 @@
-$('.nav-top .l1').mousemove(function(){
+$('.nav-top .l1').mouseenter(function(){
     $(this).find('.item').addClass('item-show');
 });
-$('.nav-top .l1').mouseout(function(){
+$('.nav-top .l1').mouseleave(function(){
     $('.nav-top .l1').find('.item').removeClass('item-show');
 });
